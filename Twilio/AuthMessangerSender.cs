668f4fdac1854eafd310851469583c094ec3549b
kelechi onu <maximillianonu@gmail.com>
2022-10-24 13:17:13 +0100

    Initial

--- Twilio/AuthMessangerSender.cs
+++ Twilio/AuthMessangerSender.cs
@@ -2,5 +2,20 @@
 {
     public class AuthMessangerSender
     {
+        public Task SendSmsAsync(string number, string message)
+        {
+            ASPSMS.SMS SMSSender = new ASPSMS.SMS();
+
+            SMSSender.Userkey = "";
+            SMSSender.Password = "";
+            SMSSender.Originator = "";
+
+            SMSSender.AddRecipient(number);
+            SMSSender.MessageData = message;
+
+            SMSSender.SendTextSMS();
+
+            return Task.FromResult(0);
+        }
     }
 }
